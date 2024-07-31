803775dca8822f777c0a6db05a76baef01f9f600
cadowtin <chasdowtin@gmail.com>
2024-07-31 17:41:18 -0500

    Moved rings added hook to put them in the array

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,17 @@
-Hooks.once('init', async function() {
-
+Hooks.once("init", async function () {
+  // Create a hook to add a custom token ring configuration. This ring configuration will appear in the settings.
+  Hooks.on("initializeDynamicTokenRingConfig", (ringConfig) => {
+    const tokenToolChainsRing = new foundry.canvas.tokens.DynamicRingData({
+      label: "Token Tool: Chains",
+      effects: {
+        RING_PULSE: "TOKEN.RING.EFFECTS.RING_PULSE",
+        RING_GRADIENT: "TOKEN.RING.EFFECTS.RING_GRADIENT",
+        BKG_WAVE: "TOKEN.RING.EFFECTS.BKG_WAVE",
+      },
+      spritesheet: "../rings/token-tool-chains-ring.json",
+    });
+    ringConfig.addConfig("tokenToolChainsRing", tokenToolChainsRing);
+  });
 });
 
-Hooks.once('ready', async function() {
-
-});
+//Hooks.once("ready", async function () {});
