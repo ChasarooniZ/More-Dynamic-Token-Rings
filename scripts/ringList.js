bf6e2a0e1e643bc43021f5c43706164cb708e712
cadowtin <chasdowtin@gmail.com>
2024-08-07 02:18:53 -0500

    Added Simple Sharp Ring

--- scripts/ringList.js
+++ scripts/ringList.js
@@ -1,10 +1,12 @@
 export const RINGS_RAW = [
   "rings-token-tool;-chains.json",
   "rings-token-tool;-runes-(black).json",
-  "rings-token-tool;-gears-(colored).json"
+  "rings-token-tool;-gears-(colored).json",
+  "rings-joeundsoo;-simple-sharp-ring.json",
 ];
 
 export const AUTHORS = [
   { name: "Token Tool", link: "https://www.rptools.net/toolbox/token-tool/" },
+  { name: "Joeundsoo", link: "https://primaryimmune.org/donate" },
   { name: "", link: "" },
 ];
