0059a554da3787059b518e5407874da029962c7f
cadowtin <chasdowtin@gmail.com>
2024-08-09 16:25:54 -0500

    Test re3deux ring

--- scripts/ringList.js
+++ scripts/ringList.js
@@ -3,12 +3,16 @@ export const RINGS_RAW = [
   "APGL/rings-token-tool;-runes-(black).json",
   "APGL/rings-token-tool;-gears-(colored).json",
   "rings-joeundsoo;-simple-sharp-ring.json",
-  "rings-chasarooni;-simply-grey-(transparent).json"
+  //"rings-chasarooni;-simply-grey-(transparent).json",
+  "rings-redeux;-simple-ring-(gradient).json",
 ];
 
 export const AUTHORS = [
   { name: "Token Tool", link: "https://www.rptools.net/toolbox/token-tool/" },
   { name: "Joeundsoo", link: "https://primaryimmune.org/donate" },
   { name: "Chasarooni", link: "https://ko-fi.com/Chasarooni" },
+  { name: "Redeux", link: "https://ko-fi.com/redeux" },
+  { name: "", link: "" },
+  { name: "", link: "" },
   { name: "", link: "" },
 ];
