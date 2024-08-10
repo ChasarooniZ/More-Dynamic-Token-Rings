9f3efcea1ba41cd376ad4594f383636d7ac2d920
cadowtin <chasdowtin@gmail.com>
2024-08-10 03:04:17 -0500

    Added Colored Ring

--- scripts/ringList.js
+++ scripts/ringList.js
@@ -6,7 +6,8 @@ export const RINGS_RAW = [
   "rings-redeux;-simple-ring.json",
   "rings-redeux;-simple-transparent-ring.json",
   "rings-redeux;-simple-ring-(clear).json",
-  "rings-eldritch-dream;-curse-ring.json"
+  "rings-eldritch-dream;-curse-ring.json",
+  "rings-eldritch-dream;-curse-ring-(colored).json",
 ];
 
 export const AUTHORS = [
