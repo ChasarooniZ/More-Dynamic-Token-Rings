e5feb6ea72aa020bf1a5c2bcf3ec6180bb655e57
cadowtin <chasdowtin@gmail.com>
2024-08-13 01:38:20 -0500

    Allow localization for ring names

--- scripts/ringHelpers.js
+++ scripts/ringHelpers.js
@@ -1,71 +1,50 @@
+import { MODULE_ID } from "./module.js";
 import { RINGS_RAW } from "./ringList.js";
 
-const default_filepath = "modules/more-dynamic-token-rings/assets/previews/";
+const DEFAULT_FILEPATH = "modules/more-dynamic-token-rings/assets/previews/";
 
 export const RINGS = RINGS_RAW.sort().map((jsonPath) => {
-  const json = formatJSON(jsonPath);
+  const json = jsonPath.split("/").pop();
   return {
     jsonPath,
     json,
     label: formatRingName(json),
     author: getAuthor(json),
-    name: formatRingName(json).replace(getAuthor(json), ''),
-    preview: getPreview(json),
-    id: getSettingId(json),
+    name: getName(json),
+    preview: `${DEFAULT_FILEPATH}${json.replace(".json", "")}-PREVIEW.webp`,
+    id: json.replace(".json", ""),
   };
 });
 
-function formatJSON(jsonPath) {
-  //split by /
-  const parts = jsonPath.split("/");
-
-  return parts[parts.length - 1];
-}
-
-/**
- * Converts a filename into a more readable token ring name
- * @param {string} filename - The input filename.
- * @returns {string} - The formatted token ring name.
- */
 function formatRingName(filename) {
-  // Remove the rings prefix
-  let name = filename.replace("rings", "");
-
-  // Remove the file extension
-  name = name.replace(".json", "");
-
-  // Replace hyphens with spaces
-  name = name.replace(/-/g, " ");
-
-  // Capitalize the first letter of each word and remove the semicolon
-  return name
+  return filename
+    .replace("rings", "")
+    .replace(".json", "")
+    .replace(/-/g, " ")
     .replace(/\b\w/g, (char) => char.toUpperCase())
     .replace(/;/g, "")
     .trim();
 }
 
 function getAuthor(filename) {
-  // Remove the file extension
-  let author = filename.replace(".json", "");
-  author = author.replace("rings", "");
-
-  //Get data before the ;
-  author = author.split(";")[0];
-
-  // Replace hyphens with spaces
-  author = author.replace(/-/g, " ");
-
-  // Capitalize the first letter of each word and remove the semicolon
-  return author.replace(/\b\w/g, (char) => char.toUpperCase()).trim();
+  return filename
+    .replace(".json", "")
+    .replace("rings", "")
+    .split(";")[0]
+    .replace(/-/g, " ")
+    .replace(/\b\w/g, (char) => char.toUpperCase())
+    .trim();
 }
 
-function getPreview(filename) {
-  // Remove the file extension
-  let name = filename.replace(".json", "");
+function getName(json) {
+  const id = json.replace(".json", "");
+  let name = formatRingName(json).replace(getAuthor(json), "");
 
-  return `${default_filepath}${name}-PREVIEW.webp`;
-}
+  if (game.i18n.lang !== "en") {
+    const local_id = `${MODULE_ID}.rings.${id}`;
+    const local = game.i18n.localize(local_id);
+    name = local !== local_id ? local : name;
+  }
 
-function getSettingId(json) {
-  return json.replace(".json", "");
+  return name;
 }
