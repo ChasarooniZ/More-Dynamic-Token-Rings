16111360da525551932bb7e950836b6e751f9fae
cadowtin <chasdowtin@gmail.com>
2024-08-30 03:29:46 -0500

    Fix import

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -1,5 +1,5 @@
 import { RINGS } from "./ringHelpers.js";
-import { MODULE_ID, effects  } from "./module.js";
+import { MODULE_BASE_PATH, MODULE_ID, effects } from "./module.js";
 import {
   getCustomRingData,
   createTokenRingDialog, validateAddCustomRing
