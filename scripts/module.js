868789533fa8190b1ed8587e251d5a768ae5d902
cadowtin <chasdowtin@gmail.com>
2024-09-02 03:30:07 -0500

    Actually set the Ring when selected

--- scripts/module.js
+++ scripts/module.js
@@ -88,35 +88,6 @@ export function getMap() {
   return ringActivationMap;
 }
 
-/**
- * 
- * @param {*} ringID ID of ring to switch to if you reload this way
- * @returns 
- */
-export async function askToReload(ringID) {
-  return new Dialog({
-    title: game.i18n.localize(MODULE_ID + ".dialog.reload.title"),
-    content: `<p>${game.i18n.localize(
-      MODULE_ID + ".dialog.reload.content"
-    )}</p>`,
-    buttons: {
-      yes: {
-        label: game.i18n.localize(
-          MODULE_ID + ".dialog.reload.buttons.yes"
-        ),
-        callback: () => {
-          foundry.utils.debouncedReload()
-        }
-      },
-      no: {
-        label: game.i18n.localize(
-          MODULE_ID + ".dialog.reload.buttons.no"
-        ),
-      },
-    },
-  }).render(true);
-}
-
 async function starterTour() {
   //tour path
   const tp = ".tours.starter.";
