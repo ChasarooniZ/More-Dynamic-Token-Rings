deb95a4c31a3bc92b139cd1020d785e62ac7007c
cadowtin <chasdowtin@gmail.com>
2024-09-03 02:21:38 -0500

    Fix issue with callign tour before localize registered

--- scripts/updates/tours/1customRingTour.js
+++ scripts/updates/tours/1customRingTour.js
@@ -1,12 +1,14 @@
-export const customRingTour = new Tour({
-    namespace: "core",
-    id: "sett-custom-ring-notification-tour",
-    title: game.i18n.localize(MODULE_ID + "tours.custom.notification.title"),
-    steps: [
-        {
-            title: game.i18n.localize(MODULE_ID + "tours.custom.notification.title"),
-            selector: '[data-tab="settings"]',
-            content: game.i18n.localize(MODULE_ID + "tours.custom.notification.step"),
-        }
-    ]
-})
+export const customRingTour = () => {
+    return new Tour({
+        namespace: "core",
+        id: "sett-custom-ring-notification-tour",
+        title: game.i18n.localize(MODULE_ID + ".tours.custom.notification.title"),
+        steps: [
+            {
+                title: game.i18n.localize(MODULE_ID + ".tours.custom.notification.title"),
+                selector: '[data-tab="settings"]',
+                content: game.i18n.localize(MODULE_ID + ".tours.custom.notification.step"),
+            }
+        ]
+    })
+}
