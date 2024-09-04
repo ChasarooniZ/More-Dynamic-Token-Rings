76a00b2da6d2f5c56c536d1885c9d182c65299ec
cadowtin <chasdowtin@gmail.com>
2024-09-03 22:56:34 -0500

    Fix import

--- scripts/updates/handleVersion.js
+++ scripts/updates/handleVersion.js
@@ -1,13 +1,14 @@
+import { MODULE_ID } from "../const.js";
 import { TOURS } from "./tourList.js";
 import { isVersionBetween } from "./versionChecker.js";
 
 export function handleVersion(oldVersion, newVersion) {
     if (!oldVersion) oldVersion = '0.0.0';
-    const toursRun = game.settings.get(MODULE_ID,"tours-run");
+    const toursRun = game.settings.get(MODULE_ID, "tours-run");
     const relevantTours = Object.keys(TOURS).filter(t => isVersionBetween(t, oldVersion, newVersion) && !toursRun.includes(t))
     for (const tour of relevantTours) {
         TOURS[tour]();
         toursRun.push(tour);
-        game.settings.set(MODULE_ID,"tours-run", toursRun)
+        game.settings.set(MODULE_ID, "tours-run", toursRun)
     }
 }
