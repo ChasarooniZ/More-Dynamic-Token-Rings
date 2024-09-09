18a7f655b248facf4f7c5380e6dc6bccc70b4372
cadowtin <chasdowtin@gmail.com>
2024-09-09 16:59:35 -0500

    Replaced intro tutorial with a chat message

--- scripts/updates/tours/1customRingNotificationTour.js
+++ scripts/updates/tours/1customRingNotificationTour.js
@@ -1,18 +1,9 @@
 import { MODULE_ID } from "../../const.js";
 
-export const customRingTour = () => {
-    const t =  new Tour({
-        namespace: "core",
-        id: "sett-custom-ring-notification-tour",
-        title: game.i18n.localize(MODULE_ID + ".tours.custom.notification.title"),
-        steps: [
-            {
-                title: game.i18n.localize(MODULE_ID + ".tours.custom.notification.title"),
-                selector: 'section#ui-middle',
-                content: game.i18n.localize(MODULE_ID + ".tours.custom.notification.step"),
-                tooltipDirection: "CENTER"
-            }
-        ]
+export const customRingTour = async () => {
+    await ChatMessage.create({
+        content: `<h2>${game.i18n.localize(MODULE_ID + ".tours.custom.notification.title")}</h2>
+    <p>${game.i18n.localize(MODULE_ID + ".tours.custom.notification.step")}</p>`,
+        whisper: ChatMessage.getWhisperRecipients("GM")
     })
-    t.start();
 }
