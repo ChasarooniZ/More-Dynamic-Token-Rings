8acd52ee6dfb327e5e5c59b94ae57edacedae025
cadowtin <chasdowtin@gmail.com>
2025-01-06 16:21:50 -0600

    revert ring movement

--- scripts/custom-ring/customRingJsonCfg.js
+++ scripts/custom-ring/customRingJsonCfg.js
@@ -111,7 +111,7 @@ export function getBaseJSON(colorBandStartRadius, colorBandEndRadius, ringColor,
         },
         "meta": {
             "version": "1.1",
-            "image": "art/custom-ring.webp",
+            "image": "custom-ring.webp",
             "format": "RGBA8888",
             "size": { "w": 7680, "h": 2048 },
             "scale": "1"
