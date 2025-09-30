1af5c04f76ace3c08f80e7b1435ef77ff481a647
cadowtin <chasdowtin@gmail.com>
2025-09-30 14:58:57 -0500

    fix chas rings

--- scripts/ringList.js
+++ scripts/ringList.js
@@ -1,34 +1,3 @@
-export const RINGS_RAW = [
-  // "APGL/rings-token-tool;-gears-(colored).json",
-  "rings-joeundsoo;-simple-sharp-ring.json",
-  "rings-redeux;-simple-ring.json",
-  "rings-redeux;-simple-transparent-ring.json",
-  "rings-redeux;-simple-ring-(clear).json",
-  "rings-eldritch-dream;-curse-ring.json",
-  "rings-eldritch-dream;-curse-ring-(colored).json",
-  //"rings-chasarooni;-runes-thin-ring-(black).json", //TODO Fix me am non readable
-  "rings-volfied;-runes-ring-(blue).json",
-  "rings-chasarooni;-chains-ring-(grey).json",
-  "rings-chasarooni;-chains-transparent-ring-(grey).json",
-  //"rings-eldritch-dream;-stop-watch-ring.json",
-  //"rings-eldritch-dream;-stop-watch-ring-(colored).json",
-  "rings-eldritch-dream;-cyber-purple-ring-(colored).json",
-  "rings-eldritch-dream;-simple-gold-ring-(colored).json",
-  "rings-eldritch-dream;-blue-aura-ring-(colored).json",
-  "rings-eldritch-dream;-bronze-spiral-ring-(colored).json",
-  "rings-eldritch-dream;-dual-glow-ring-(colored).json", //TODO FIX
-  "rings-eldritch-dream;-pink-metal-ring-(colored).json",
-  "rings-eldritch-dream;-classy-black-ring.json",
-  "rings-volfied;-runes-ring-(black).json",
-  "rings-eldritch-dream;-gilded-black-marble-ring.json",
-  "rings-chasarooni;-abomination-vaults-ring.json",
-  "rings-eldritch-dream;-gilded-black-marble-ring-(colored).json",
-  "rings-chasarooni;-ruby-phoenix-ring-(colorable).json",
-  "rings-volfied;-chiaroscuro.json",
-  "rings-volfied;-warpgate.json",
-  "rings-gensokian;-toon-marble.json"
-];
-
 export const RINGS = {
   "Chasarooni": [
     "abomination-vaults-ring-(colorable)",
